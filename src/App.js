2bb6c9f6ad178eea46f4f6b25a993eeb0988101e
chartwell85 <chartwell85@gmail.com>
2025-07-02 13:39:38 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🦆 Waterfowl Pro</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
